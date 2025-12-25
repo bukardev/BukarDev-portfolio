9e3149d3327288226134728c86cc16dbaa1acf80
Bukar <011abuakarlawan@gmail.com>
2025-12-25 03:22:10 -1100

    Add desktop optimized project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -47,7 +47,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-15">
+    <section className="pb-15 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
@@ -55,7 +55,7 @@ export const ProjectsSection = () => {
           </p>
         </div>
 
-        <h2 className="font-serif text-3xl  md:text-5xl mt-5 text-center">
+        <h2 className="font-serif text-3xl  md:text-5xl mt-5 text-center lg:text-xl">
           Featured Projects
         </h2>
         <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto">
@@ -66,7 +66,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8 md:pt-12 md:px-10"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5"
@@ -74,37 +74,43 @@ export const ProjectsSection = () => {
                   backgroundImage: `url(${grainImage.src})`,
                 }}
               ></div>
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400  inline-flex gap-2 uppercase tracking-widest font-bold text-sm bg-clip-text text-transparent">
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400  inline-flex gap-2 uppercase tracking-widest font-bold text-sm bg-clip-text text-transparent">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li
+                        key={result.title}
+                        className="flex gap-2 text-white/50 text-sm md:text-base "
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 hover:bg-white/55 transition px-6 md:w-auto">
+                      <span>Visie Live Site</span>
+                      <ArrowUprightIcon className="size-5" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
               </div>
-              <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
-                {project.title}
-              </h3>
-              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                {project.results.map((result) => (
-                  <li
-                    key={result.title}
-                    className="flex gap-2 text-white/50 text-sm md:text-base "
-                  >
-                    <CheckCircleIcon className="size-5 md:size-6" />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 hover:bg-white/55 transition px-6 md:w-auto">
-                  <span>Visie Live Site</span>
-                  <ArrowUprightIcon className="size-5" />
-                </button>
-              </a>
-              <Image
-                src={project.image}
-                alt={project.title}
-                className="mt-8 -mb-4 md:-mb-0"
-              />
             </div>
           ))}
         </div>
