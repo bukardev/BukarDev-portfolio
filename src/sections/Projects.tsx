e43d226cf1f2faf849c53a51e624b5b05e2398c6
Bukar <011abuakarlawan@gmail.com>
2025-12-25 02:08:12 -1100

    Add prjects section but mobile optimized

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import CheckCircleIcon from "@/assets/icons/Check-circle.svg";
+import ArrowUprightIcon from "@/assets/icons/Arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -42,5 +46,67 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-15">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+            Real-world Results
+          </p>
+        </div>
+
+        <h2 className="font-serif text-3xl mt-5 text-center">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4">
+          See how our projects have delivered real-world results for our
+          clients.
+        </p>
+        <div className="flex flex-col mt-8 gap-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400  inline-flex gap-2 uppercase tracking-widest font-bold text-sm bg-clip-text text-transparent">
+                <span>{project.company}</span>
+                <span>&bull;</span>
+                <span>{project.year}</span>
+              </div>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
+              <ul className="flex flex-col gap-4 mt-4">
+                {project.results.map((result) => (
+                  <li
+                    key={result.title}
+                    className="flex gap-2 text-white/50 text-sm"
+                  >
+                    <CheckCircleIcon className="size-5" />
+                    <span>{result.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 hover:bg-white/55 transition">
+                  <span>Visite Live Site</span>
+                  <ArrowUprightIcon className="size-5" />
+                </button>
+              </a>
+              <Image
+                src={project.image}
+                alt={project.title}
+                className="mt-8 -mb-4"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
