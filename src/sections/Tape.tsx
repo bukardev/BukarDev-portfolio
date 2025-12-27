97fa0de5a3d893156c4ff7f321a131a4f89b566d
Bukar <011abuakarlawan@gmail.com>
2025-12-26 23:32:41 -1100

    Add tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,38 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Scalable",
+  "Reliable",
+  "Secure",
+  "Innovative",
+  "User-Friendly",
+  "Customizable",
+  "Efficient",
+  "Robust",
+  "Versatile",
+  "Responsive",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <section>
+      <div className="my-16 overflow-x-clip">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3">
+          <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+            <div className="flex flex-none gap-4 py-3">
+              {words.map((word) => (
+                <div key={word} className="inline-flex gap-4 items-center">
+                  <span className="text-gray-900 uppercase font-extrabold text-sm">
+                    {word}
+                  </span>
+                  <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
