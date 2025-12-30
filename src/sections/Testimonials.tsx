51ecb8e9c3a1d8277796d9a5248e21afeeb71c86
Bukar <011abuakarlawan@gmail.com>
2025-12-30 02:13:42 -1100

    Complete testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <section className="my-16">
+    <section className="my-16 lg:my-24">
       <div className="container">
         <div>
           <SectionHeader
@@ -52,19 +52,34 @@ export const TestimonialsSection = () => {
             description={`Don't just take our word for it. Hear what our satisfied clients have to say about their experience working with us.`}
           />
         </div>
-        <div className="flex flex-col mt-8 gap-20">
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <div className="flex items-center">
-                <Image src={testimonial.avatar} alt={testimonial.name} />
-                <div className="flex flex-col">
-                  <div className="text-sm">{testimonial.name}</div>
-                  <div className="text-sm">{testimonial.position}</div>
+        <div className="flex mt-16 overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-24">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card
+                key={testimonial.name}
+                className="max-w-xs md:max-w-md md:p-8"
+              >
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+                <p className="mt-4 text-sm md:text-base md:mt-6">
+                  {testimonial.text}
+                </p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </section>
