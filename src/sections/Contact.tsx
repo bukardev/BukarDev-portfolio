da6a61aa008ee9af958bde21a1e4882dffb0e021
Bukar <011abuakarlawan@gmail.com>
2026-01-01 22:48:28 -1100

    Add contact section mobile view

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,30 @@
+import ArrowUprightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <section className="my-16">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <h2 className="font-serif font-semibold text-2xl">{`Let's create something amazing together`}</h2>
+          <p className="text-sm mt-2">
+            Ready to bring your next project to life? lets connect and discuss
+            how I can help you achieve your goals
+          </p>
+          <a href="#">
+            <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8">
+              <span className="font-semibold">Contact Me</span>
+              <ArrowUprightIcon className="size-4" />
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
