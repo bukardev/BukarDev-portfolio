8198cf23a0ac032ea3249bbc80ff4c547725708f
Bukar <011abuakarlawan@gmail.com>
2026-01-04 02:28:06 -1100

    Add animation for testimonial section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { Fragment } from "react";
 
 const testimonials = [
   {
@@ -43,8 +44,8 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <section className="my-16 lg:my-24">
-      <div className="container">
+    <section className="py-16 lg:py-24">
+      <div className="container sm:px-[1px]">
         <div>
           <SectionHeader
             eyebrow="Happy Clients"
@@ -52,33 +53,41 @@ export const TestimonialsSection = () => {
             description={`Don't just take our word for it. Hear what our satisfied clients have to say about their experience working with us.`}
           />
         </div>
-        <div className="flex mt-16 overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-24">
-          <div className="flex gap-8 flex-none">
-            {testimonials.map((testimonial) => (
-              <Card
-                key={testimonial.name}
-                className="max-w-xs md:max-w-md md:p-8"
-              >
-                <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
-                    <Image
-                      src={testimonial.avatar}
-                      alt={testimonial.name}
-                      className="max-h-full"
-                    />
-                  </div>
-                  <div>
-                    <div className="font-semibold">{testimonial.name}</div>
-                    <div className="text-sm text-white/40">
-                      {testimonial.position}
-                    </div>
-                  </div>
-                </div>
-                <p className="mt-4 text-sm md:text-base md:mt-6">
-                  {testimonial.text}
-                </p>
-              </Card>
-            ))}
+        <div className="flex mt-12 overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 lg:mt-20">
+          <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+            {[
+              ...new Array(2).fill(0).map((_, index) => (
+                <Fragment key={index}>
+                  {testimonials.map((testimonial) => (
+                    <Card
+                      key={testimonial.name}
+                      className="max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300"
+                    >
+                      <div className="flex gap-4 items-center">
+                        <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                          <Image
+                            src={testimonial.avatar}
+                            alt={testimonial.name}
+                            className="max-h-full"
+                          />
+                        </div>
+                        <div>
+                          <div className="font-semibold">
+                            {testimonial.name}
+                          </div>
+                          <div className="text-sm text-white/40">
+                            {testimonial.position}
+                          </div>
+                        </div>
+                      </div>
+                      <p className="mt-4 text-sm md:text-base md:mt-6">
+                        {testimonial.text}
+                      </p>
+                    </Card>
+                  ))}
+                </Fragment>
+              )),
+            ]}
           </div>
         </div>
       </div>
